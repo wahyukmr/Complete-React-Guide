816c6c62096dfcd2e8bb0e00f5ea0355464e8290
wahyukmr <maryu9229@gmail.com>
2023-02-21 14:26:34 +0700

    Connecting redux toolkit state

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { legacy_createStore as createStore } from "redux";
+// configureStore: seperti createStore, tetapi itu membuat penggabungan multiple reducer menjadi satu reducer. Meneruskan sebuah objek bukan fungsi reducer
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-// createSlice menerima object sebagai argumennya, yang dilakukan createSlice adalah memprsiapkan potongan Global State. Jika memiliki bagian yang berbeda atau yang tidak terkait langsung dengan potongan State, kita dapat membuat potongan lain yang berbeda. Setiap irisan membutuhkan property nama sebagai pengenal dari bagian State tersebut dan property reducers yang bisa dibilang seperti map() dari semua reducer yang dibutuhkan potongan State itu sendiri.
-createSlice({
+const counterSlice = createSlice({
     name: "counter",
     initialState,
     reducers: {
-        // disetiap method ini secara otomatis menerima State terbaru dan mengijinkan untuk mengubah State tersebut tanpa harus menyalin dan membuat objek atau array baru karena dengan redux toolkit secara otomatis mengkloning State yang ada dan membuat objek atau array State baru
         increment(state, action) {
             state.counter += action.amount;
         },
@@ -21,31 +19,8 @@ createSlice({
     },
 });
 
-// function Reducer
-const counterReducer = (state = initialState, action) => {
-    if (action.type === "increment") {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter,
-        };
-    }
-    if (action.type === "decrement") {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter,
-        };
-    }
-    if (action.type === "toggle") {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter,
-        };
-    }
-
-    return state;
-};
-
-// New store
-const stateStorage = createStore(counterReducer);
+const stateStorage = configureStore({
+    reducer: counterSlice.reducer,
+});
 
 export default stateStorage;
