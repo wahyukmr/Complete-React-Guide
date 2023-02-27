cf1725b7add16337b61f0c963e6240a3a2b7bfce
wahyukmr <maryu9229@gmail.com>
2023-02-27 10:29:28 +0700

    Navigating between pages with Link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
+// Yang dilakukan komponen Link dibawah adalah merender anchor element, namun pada dasarnya mencegah browser secara default mengirimkan HTTP Request jika diklik dan sebagai gantinya akan memperbarui halaman dan konten yang dituju.
 function HomePage() {
-    return <h1>My Home Page</h1>;
+    return (
+        <>
+            <h1>My Home Page</h1>
+            <p>
+                Go to <Link to="/products">products</Link>
+            </p>
+        </>
+    );
 }
 
 export default HomePage;
