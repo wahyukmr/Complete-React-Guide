72d8501ecfa92afe4b6f73e2622c396862725a84
wahyukmr <maryu9229@gmail.com>
2023-02-27 08:40:45 +0700

    update fatching data

--- src/store/cart-thunk.js
+++ src/store/cart-thunk.js
@@ -3,19 +3,15 @@ import { uiActions } from "./ui-slice";
 
 export const fetchCartData = () => {
     return async (dispatch) => {
-        const fetchData = async () => {
+        try {
             const response = await fetch(
                 "https://react-project1-4c0ab-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json"
             );
             if (!response.ok) {
                 throw new Error("couldn't fetch cart data");
             }
-            const cartDataResult = response.json();
-            return cartDataResult;
-        };
+            const cartData = await response.json();
 
-        try {
-            const cartData = await fetchData();
             dispatch(
                 cartActions.replaceCart({
                     items: cartData?.items || [],
