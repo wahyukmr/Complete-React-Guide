69fb9956414cc0b3eace0aa21da65cf44e5ffc4d
wahyukmr <maryu9229@gmail.com>
2023-03-06 16:45:36 +0700

    Outomatic logout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,7 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 export default function RootLayout() {
+    const token = useLoaderData();
+    const submit = useSubmit();
+
+    useEffect(() => {
+        if (!token) return;
+
+        setTimeout(() => {
+            submit(null, { action: "/logout", method: "POST" });
+        }, 1 * 60 * 60 * 1000);
+    }, [token, submit]);
+
     return (
         <>
             <MainNavigation />
