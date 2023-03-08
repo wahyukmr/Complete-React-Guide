e9ee411c7da4492b54878b2674dd05f03ec3bb02
wahyukmr <maryu9229@gmail.com>
2023-03-09 02:53:03 +0700

    Using programmatic navigation

--- nextjs/components/meetups/MeetupItem.js
+++ nextjs/components/meetups/MeetupItem.js
@@ -1,7 +1,16 @@
+import { useRouter } from "next/router";
 import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
 
 export default function MeetupItem(props) {
+    // Dengan useRouter akan mendapatkan akses objek router yang dapat digunakan untuk menunjukkan detail handler untuk menavigasi secara terprogram.
+    const router = useRouter();
+
+    function showDetailHandler() {
+        // push method akan mendorong halaman baru ke tumpukan halaman dan itu seperti komponen Link, tetapi untuk menavigasi secara terprogram.
+        router.push(`/${props.id}`);
+    }
+
     return (
         <li className={classes.item}>
             <Card>
@@ -13,7 +22,7 @@ export default function MeetupItem(props) {
                     <address>{props.address}</address>
                 </div>
                 <div className={classes.actions}>
-                    <button>Show Details</button>
+                    <button onClick={showDetailHandler}>Show Details</button>
                 </div>
             </Card>
         </li>
